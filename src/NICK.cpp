5508bc6c9beda3075410a074b55ca622e75f3b4c
Alexis Abda <alexis.abda@hotmail.com>
2024-04-24 06:08:19 +0200

    fix map on socket,client and map on string,client

--- src/NICK.cpp
+++ src/NICK.cpp
@@ -1,6 +1,8 @@
 #include "../include/Server.hpp"
 
 void	Server::_nick(int socket, std::vector<std::string>& arg, Client cl) {
+	std::string	newNick;
+
 	if (!cl.getIsRegistred()) {
 		std::stringstream	ss;
 		std::string			socketNbr;
@@ -8,6 +10,10 @@ void	Server::_nick(int socket, std::vector<std::string>& arg, Client cl) {
 		if (ss.fail())
 			throw std::runtime_error("stringstream failed !");
 		ss >> socketNbr;
-		cl.setNickname(arg[0] + socketNbr);
+		newNick = arg[0] + socketNbr;
+		this->_mapSocketAndClients[socket].setNickname(newNick);
+		this->_mapNicknameAndClients[newNick] = this->_mapSocketAndClients[socket];
+	} else {
+		
 	}
 }
