1ce17cc2113a3345cf200c06abc1523b170a0ab0
Gaetan <gaetan.marchale@hotmail.com>
2024-04-25 20:27:52 +0200

    432 ERR_ERRONEUSNICKNAME

--- src/NICK.cpp
+++ src/NICK.cpp
@@ -1,16 +1,34 @@
 #include "../include/Server.hpp"
 
-void	Server::_checkIfNickAlreadyTaken(std::string newNick, Client cl) {
-	std::map<std::string, Client>::iterator it;
+bool	Server::_nickAlreadyTaken(std::string newNick, Client cl) {
+	std::map<std::string, Client>::const_iterator it;
 	for (it = this->_mapNicknameAndClients.begin(); it != this->_mapNicknameAndClients.end(); it++) {
-		if (newNick == it->first)
+		if (newNick == it->first) {
 			cl.sendMessage(ERR_NICKNAMEINUSE(newNick));			
+			return true;
+		}
 	}
+	return false;
+}
+
+bool	Server::_isValidNickname(std::string nickname, Client cl) {
+	if (nickname.length() > 9) {
+		cl.sendMessage(ERR_ERRONEUSNICKNAME());
+		return false;
+	}
+	else if (nickname == "anonymous") {
+		cl.sendMessage(ERR_ERRONEUSNICKNAME());
+		return false;
+	}
+	return true;
 }
 
 void	Server::_nick(int socket, std::vector<std::string>& arg, Client cl) {
 	if (!cl.getIsRegistered()) {
-		this->_checkIfNickAlreadyTaken(arg[0], cl);
+		if (this->_isValidNickname(arg[0], cl) == 0)
+			return ;
+		if (this->_nickAlreadyTaken(arg[0], cl) == 1)
+			return ;
 		std::stringstream	ss;
 		std::string			socketNbr;
 		ss << socket;
