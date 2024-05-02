855ae3272b573051669e3bfd67ffd09d3f94d41f
Alexis Abda <alexis.abda@hotmail.com>
2024-05-02 22:58:48 +0200

    error message if incorrect password

--- include/err.hpp
+++ include/err.hpp
@@ -6,4 +6,4 @@
 #define	ERR_NONICKNAMEGIVEN(nickname) (std::string(":") + SERVER_NAME + " 431 *" + nickname + " :No nickname given\r\n")
 #define	ERR_ERRONEUSNICKNAME(reason) ("432 :Erroneus nickname\r\n")
 #define	ERR_NICKNAMEINUSE(nick) ("433 " + nick + " :Nickname is already in use\r\n")
-#define ERR_PASSWDMISMATCH(nickname) (std::string(":") + SERVER_NAME + " 464 " + nickname + " :Password incorrect\r\n")
+#define ERR_PASSWDMISMATCH(nickname) (std::string(":") + SERVER_NAME + " 464 " + nickname + SERVER_NAME + " :Password incorrect" + "\r\n")
--- src/PASS.cpp
+++ src/PASS.cpp
@@ -1,9 +1,10 @@
 #include "../include/Server.hpp"
+#include "../include/config.hpp"
 
 void	Server::_pass(int socket, std::vector<std::string>& arg, Client cl) {
 	static_cast<void>(cl);
 	if (arg[0] == this->_password)
 		this->_mapSocketAndClients[socket].setPassword(arg[0]);
 	else
-		this->_mapSocketAndClients[socket].sendMessage(ERR_PASSWDMISMATCH());
+		this->_mapSocketAndClients[socket].sendMessage(ERR_PASSWDMISMATCH(this->_mapSocketAndClients[socket].getNickname()));
 }
