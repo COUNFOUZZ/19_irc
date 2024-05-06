a84a498160b87fd6108858cd8ed2010d6991c669
Alexis Abda <alexis.abda@hotmail.com>
2024-05-06 16:14:04 +0200

    fix segfault

--- include/err.hpp
+++ include/err.hpp
@@ -6,4 +6,5 @@
 #define	ERR_NONICKNAMEGIVEN(nickname) (std::string(":") + SERVER_NAME + " 431 *" + nickname + " :No nickname given\r\n")
 #define	ERR_ERRONEUSNICKNAME(reason) ("432 :Erroneus nickname\r\n")
 #define	ERR_NICKNAMEINUSE(nick) ("433 " + nick + " :Nickname is already in use\r\n")
+#define ERR_NEEDMOREPARAMS(reason) (std::string("461 ") + " :" + reason + "\r\n")
 #define ERR_PASSWDMISMATCH(nickname) (std::string(":") + SERVER_NAME + " 464 " + nickname + SERVER_NAME + " :Password incorrect" + "\r\n")
--- src/USER.cpp
+++ src/USER.cpp
@@ -2,6 +2,10 @@
 
 void	Server::_user(int socket, std::vector<std::string>& arg, Client cl) {
 	static_cast<void>(cl);
+	if (arg.size() != 4) {
+		this->_mapSocketAndClients[socket].sendMessage(ERR_NEEDMOREPARAMS("Invalid USER command !"));
+		return;
+	}
 	this->_mapSocketAndClients[socket].setUsername(arg[0]);
-	this->_mapSocketAndClients[socket].setHostname(arg[2]);
+	this->_mapSocketAndClients[socket].setHostname(arg[1]);
 }
