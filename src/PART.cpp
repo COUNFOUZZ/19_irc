8b3f59a2c4b682b4763f4229d03aa4012c6d310d
Alexis Abda <alexis.abda@hotmail.com>
2024-05-15 01:15:30 +0200

    fix problem can part twice time

--- src/PART.cpp
+++ src/PART.cpp
@@ -10,7 +10,9 @@ void	Server::_part(int socket, std::vector<std::string>& arg, Client cl) {
 			buffer += arg[i] + ' ';
 		buffer.erase(buffer.size() - 1);
 		msg += buffer + "\r\n";
-		this->_channels[arg[0]].eraseUserFromChannel(this->_mapSocketAndClients[socket].getNickname());
-		this->_channels[arg[0]].broadcast(msg, this->_mapSocketAndClients[socket]);
+		if (this->_channels[arg[0]].isUserIsInChannel(this->_mapSocketAndClients[socket].getNickname())) {
+			this->_channels[arg[0]].eraseUserFromChannel(this->_mapSocketAndClients[socket].getNickname());
+			this->_channels[arg[0]].broadcast(msg, this->_mapSocketAndClients[socket]);
+		}
 	}
 }
