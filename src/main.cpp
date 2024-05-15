20f4fcb061f51ba5692db5bd084feaba6251febe
Gaetan <gaetan.marchale@hotmail.com>
2024-05-15 19:33:01 +0200

    Deleted tests in the main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ static void	shutdown(int) {
 	g_shutdown = true;
 }
 
-/*
 int	main(int argc, char **argv) {
 	if (argc != 3) {
 		return std::cerr << "Invalid number of arguments (./ircserv <port> <password>)" << std::endl, 1;
@@ -21,38 +20,4 @@ int	main(int argc, char **argv) {
 		std::cerr << "Error: " << e.what() << std::endl;
 	}
 	return 0;
-}
-*/
-
-int main()
-{
-	Client cli;
-	Channel chan;
-
-	shutdown(1);
-	if (g_shutdown == false)
-	{
-		std::cout << "Test" << std::endl;
-	}
-
-	std::cout << "userModes : " << cli.getUserModes() << std::endl;
-	std::cout << "channelModes : " << chan.getChannelModes() << std::endl;
-
-	cli.setUserModes("itkii");
-	chan.setChannelModes("tki");
-
-	std::cout << "userModes : " << cli.getUserModes() << std::endl;
-	std::cout << "channelModes : " << chan.getChannelModes() << std::endl;
-
-	if (cli.getUserModes().find("i") == cli.getUserModes().npos)
-	{
-		std::cout << "No i" << std::endl;
-	}
-	else
-		std::cout << "Yes i" << std::endl;
-
-	cli.delUserModes('i');
-	chan.delChannelModes('i');
-	std::cout << "userModes : " << cli.getUserModes() << std::endl;
-	std::cout << "channelModes : " << chan.getChannelModes() << std::endl;
 }
