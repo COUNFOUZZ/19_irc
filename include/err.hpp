fb36ebc4def50de9f7c52de0deecbcac5c2545f5
Alexis Abda <alexis.abda@hotmail.com>
2024-05-15 02:03:49 +0200

    PART

--- include/err.hpp
+++ include/err.hpp
@@ -8,6 +8,7 @@
 #define	ERR_NONICKNAMEGIVEN(nickname) (std::string(":") + SERVER_NAME + " 431 *" + nickname + " :No nickname given\r\n")
 #define	ERR_ERRONEUSNICKNAME(nickname) ("432 " + nickname + " :Erroneus nickname\r\n")
 #define	ERR_NICKNAMEINUSE(nick) ("433 " + nick + " :Nickname is already in use\r\n")
+#define ERR_NOTONCHANNEL(nickname, channel) (std::string(":") + SERVER_NAME + " 442 " + nickname + " " + channel + " :You are not on that channel" + "\r\n")
 #define ERR_NEEDMOREPARAMS(reason) (std::string("461 :") + reason + "\r\n")
 #define ERR_ALREADYREGISTERED(reason) (std::string("462 :") + reason + "\r\n")
 #define ERR_PASSWDMISMATCH(nickname) (std::string(":") + SERVER_NAME + " 464 " + nickname + SERVER_NAME + " :Password incorrect" + "\r\n")
--- src/PART.cpp
+++ src/PART.cpp
@@ -2,17 +2,25 @@
 
 void	Server::_part(int socket, std::vector<std::string>& arg, Client cl) {
 	static_cast<void>(cl);
-	if (arg.size() >= 1) {
-		std::string	msg(this->_mapSocketAndClients[socket].getPrefix() + " PART ");
-		std::string	buffer;
-
-		for (size_t i = 0; i < arg.size(); ++i)
-			buffer += arg[i] + ' ';
-		buffer.erase(buffer.size() - 1);
-		msg += buffer + "\r\n";
-		if (this->_channels[arg[0]].isUserIsInChannel(this->_mapSocketAndClients[socket].getNickname())) {
-			this->_channels[arg[0]].eraseUserFromChannel(this->_mapSocketAndClients[socket].getNickname());
-			this->_channels[arg[0]].broadcast(msg, this->_mapSocketAndClients[socket]);
-		}
+	if (arg.size() < 1){
+		this->_mapSocketAndClients[socket].sendMessage(ERR_NEEDMOREPARAMS("PART"));
+        return;
 	}
+	if (this->_channels.find(arg[0]) == this->_channels.end()) {
+		this->_mapSocketAndClients[socket].sendMessage(ERR_NOSUCHCHANNEL(this->_mapSocketAndClients[socket].getNickname(), arg[0]));
+		return;
+	}
+	if (!this->_channels[arg[0]].isUserIsInChannel(this->_mapSocketAndClients[socket].getNickname())) {
+		this->_mapSocketAndClients[socket].sendMessage(ERR_NOTONCHANNEL(this->_mapSocketAndClients[socket].getNickname(), arg[0]));
+		return;
+	}
+	std::string	msg(this->_mapSocketAndClients[socket].getPrefix() + " PART ");
+	std::string	buffer;
+
+	for (size_t i = 0; i < arg.size(); ++i)
+		buffer += arg[i] + ' ';
+	buffer.erase(buffer.size() - 1);
+	msg += buffer + "\r\n";
+		this->_channels[arg[0]].eraseUserFromChannel(this->_mapSocketAndClients[socket].getNickname());
+		this->_channels[arg[0]].broadcast(msg, this->_mapSocketAndClients[socket]);
 }
