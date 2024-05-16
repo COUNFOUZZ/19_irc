880849aa93011bbd8e24dd6113ea00651749d220
Alexis Abda <alexis.abda@hotmail.com>
2024-05-16 04:34:34 +0200

    add OPERATOR pass

--- src/PASS.cpp
+++ src/PASS.cpp
@@ -1,9 +1,26 @@
 #include "../include/Server.hpp"
 
+int	Server::_whichPassword(std::string pass) {
+	if (pass == this->_password)
+		return 1;
+	else if (pass == OPERATOR_PASSWORD)
+		return 2;
+	return 0;
+}
+
 void	Server::_pass(int socket, std::vector<std::string>& arg, Client cl) {
 	static_cast<void>(cl);
-	if (arg[0] == this->_password)
-		this->_mapSocketAndClients[socket].setPassword(arg[0]);
-	else
-		this->_mapSocketAndClients[socket].sendMessage(ERR_PASSWDMISMATCH(this->_mapSocketAndClients[socket].getNickname()));
+	int	check = this->_whichPassword(arg[0]);
+
+	switch (check) {
+		case 1:
+			this->_mapSocketAndClients[socket].setPassword(arg[0]);
+			break;
+		case 2:
+			this->_mapSocketAndClients[socket].setPassword(arg[0]);
+			this->_mapSocketAndClients[socket].setServerOP(true);
+			break;
+		default:
+			this->_mapSocketAndClients[socket].sendMessage(ERR_PASSWDMISMATCH(this->_mapSocketAndClients[socket].getNickname()));
+	}
 }
