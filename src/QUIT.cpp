10569d9696b7efa41edb779be372771886ee126f
Alexis Abda <alexis.abda@hotmail.com>
2024-05-17 02:47:21 +0200

    delete useless line

--- src/QUIT.cpp
+++ src/QUIT.cpp
@@ -1,7 +1,6 @@
 #include "../include/Server.hpp"
 
 void	Server::_quit(int socket, std::vector<std::string>& arg, Client cl) {
-	static_cast<void>(arg);
 	static_cast<void>(cl);
 	std::cout << "[" << socket << "]" << this->_mapSocketAndClients[socket].getNickname() << ": CLIENT QUIT THE SERVER" << std::endl;
 	if (this->_mapSocketAndClients[socket].nbrOfActiveChannel()) {
