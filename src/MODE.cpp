824fafef13ce0ca0e71a6fcba19d93dc9972ee3e
Alexis Abda <alexis.abda@hotmail.com>
2024-05-18 00:50:40 +0200

    remove dirty debug

--- src/MODE.cpp
+++ src/MODE.cpp
@@ -9,10 +9,6 @@ void	Server::_mode(int socket, std::vector<std::string>& arg, Client cl) {
 		right = arg[1];
 		user = arg[2];
 
-		std::cout << "debug cmd: " << std::endl << "arg[0] = " << channelName << std::endl
-												<< "arg[1] = " << right << std::endl
-												<< "arg[2] = " << user << std::endl;
-
 		if (!channelName.empty() && this->_userExist(user)) {
 			if (this->_channels.find(channelName) != this->_channels.end() && (this->_mapSocketAndClients[socket].getServerOP() || this->_channels[channelName].isInOpVector(this->_mapSocketAndClients[socket].getNickname()))) {
 				std::map<int, Client>::iterator it;
@@ -21,20 +17,15 @@ void	Server::_mode(int socket, std::vector<std::string>& arg, Client cl) {
 						break;
 				switch (right[0]) {
 					case '+':
-						std::cout << "+ TRIGGERED with right: " << right << std::endl;
 						right.erase(0, 1);
-						std::cout << "right after erase: " << right << std::endl;
 						for (size_t	i = 0; i < right.size(); ++i)
 							it->second.addChannelAndRight(channelName, right[i]);
 						break;
 					case '-':
-						std::cout << "- TRIGGERED" << std::endl;
 						right.erase(0, 1);
 						for (size_t	i = 0; i < right.size(); ++i)
 							it->second.delARightFromChannelAndRight(channelName, right[i]);
 						break;
-					default:
-						std::cout << "error" << std::endl;
 				}
 				std::cout << "USER: " << it->second.getNickname() << " [" << it->second.getRightFromChannel(channelName) << "]" << std::endl;
 			}
