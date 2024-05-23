db726ddd84c40dbde77ca625c30f096360cf6125
Alexis Abda <alexis.abda@hotmail.com>
2024-05-23 12:28:14 +0200

    fix semantic

--- src/MOTD.cpp
+++ src/MOTD.cpp
@@ -3,10 +3,9 @@
 void	Server::_motd(int socket, std::vector<std::string>& arg) {
 	if (arg.size() > 0)
 		this->_mapSocketAndClients[socket].sendMessage(ERR_NOSUCHSERVER(arg[1]));
-	else if (!_messageOfTheDay.empty())
+	else if (!this->_messageOfTheDay.empty())
 		this->_mapSocketAndClients[socket].sendMessage(RPL_MOTD(this->_messageOfTheDay));
 	else
 		this->_mapSocketAndClients[socket].sendMessage(ERR_NOMOTD("There is no message of the day."));
-
 }
 
